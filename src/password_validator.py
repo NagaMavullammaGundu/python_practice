7c8a3d685f25894f825ab34e82fa92264c8ce191
NagaMavullammaGundu <gundunm16@gmail.com>
2024-12-28 18:38:24 +0530

    Improved code with RegEx

--- src/password_validator.py
+++ src/password_validator.py
@@ -1,7 +1,6 @@
+import re
 class PasswordValidator:
-    def __init__(self):
-        print("password validator object is craeted")
-
+    
     def input_password(self):
         self.password = input("Create new password : ")
 
@@ -9,31 +8,16 @@ class PasswordValidator:
         if not len(self.password) >= 8:
             raise ValueError("your password should be atleast 8 characters long")
 
-    def check_upperletters(self):
-        upperflag = True
-        for i in self.password:
-            if "A" <= i <= "Z":
-                upperflag = False
-                break
-        if upperflag:
+    def check_upperletters(self):   
+        if not re.match(r'.*[A-Z].*',self.password):
             raise TypeError("your password should contain atleast one upper character")
 
     def check_lowerletters(self):
-        lowerflag = True
-        for j in self.password:
-            if "a" <= j <= "z":
-                lowerflag = False
-                break
-
-        if lowerflag:
+        if not re.match(r'.*[a-z].*',self.password):
             raise TypeError("your password should contain atleast one lower character")
 
     def check_numbers(self):
-        numberflag = True
-        for i in self.password:
-            if ord(i) >= 48 and ord(i) <= 57:
-                numberflag = False
-        if numberflag:
+        if not re.match(r'.*[0-9].*',self.password):
             raise TypeError("your password should contain atleast one number")
 
     def validate_password(self):
