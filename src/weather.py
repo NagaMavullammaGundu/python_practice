ff0854af451902de227301389c9b1489ec487986
NagaMavullammaGundu <gundunm16@gmail.com>
2025-01-04 20:41:44 +0530

    used open weather map API

---
+++ src/weather.py
@@ -0,0 +1,18 @@
+import requests
+
+city_name = input("Enter the city name : ")
+api_key = "b3fcc4af13e5e9124f915c36d99ff956"
+URL = "http://api.openweathermap.org/data/2.5/weather?"
+
+complete_url = URL + "appid=" + api_key + "&q=" + city_name
+
+response = requests.get(complete_url)
+data = response.json()
+
+
+if data["cod"] != "404":
+    print(f"City Name : {city_name}")
+    print(f"Minimum Temperature : {data['main']['temp_min']} F")
+    print(f"Maximum Temperature : {data['main']['temp_max']} F")
+else:
+    raise ValueError(f"Given city name {city_name} is invalid")
